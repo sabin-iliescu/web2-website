dcf0029f5d8da2aedf04cc309ebbe29c21e7d877
lucianpopa84 <lucianpopa84@gmail.com>
2018-11-03 11:06:11 +0200

    Added cake page

---
+++ main.js
@@ -0,0 +1,10 @@
+function addToCart() {
+    var selectedQuantity = inputGroupSelect04.options[inputGroupSelect04.selectedIndex].value;
+    window.console.log("selectedQuantity: ", selectedQuantity);
+    if (selectedQuantity==1 || selectedQuantity==2 || selectedQuantity==3) {
+        document.getElementById("textbox").innerHTML = [selectedQuantity.toString()," kg cake added to cart!"]; 
+    } else {
+        document.getElementById("textbox").innerHTML = ""; 
+    }
+    
+  }
