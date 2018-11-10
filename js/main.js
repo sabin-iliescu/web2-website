757843b513b03692d7ac2f97c149bc28585d1634
lucianpopa84 <lucianpopa84@gmail.com>
2018-11-10 08:15:26 +0200

    Separated cake.css and cake.js from main and styling modifications

--- js/main.js
+++ js/main.js
@@ -43,17 +43,6 @@ function addToggle() {
 
 document.addEventListener('DOMContentLoaded', addToggle);
 
-// Shopping cart functionality
-function addToCart() {
-    var selectedQuantity = inputGroupSelect04.options[inputGroupSelect04.selectedIndex].value;
-    window.console.log("selectedQuantity: ", selectedQuantity);
-    if (selectedQuantity == 1 || selectedQuantity == 2 || selectedQuantity == 3) {
-        document.getElementById("textbox").innerHTML = [selectedQuantity.toString(), " kg cake added to cart!"];
-    } else {
-        document.getElementById("textbox").innerHTML = "";
-    }
-
-}
 
 //  Form validation methods
 function validateName() {
