0732ae8ab7d283349b0503eb74d0e9da575fd635
lucianpopa84 <lucianpopa84@gmail.com>
2018-11-10 13:37:57 +0200

    Merge branch 'Lucian'
    # Conflicts:
    #	css/main.css
    #	pages/cake.html

--- js/main.js
+++ js/main.js
@@ -42,17 +42,6 @@
     document.addEventListener('scroll', respondToScrollEvent);
 })();
 
-// Shopping cart functionality
-function addToCart() {
-    var selectedQuantity = inputGroupSelect04.options[inputGroupSelect04.selectedIndex].value;
-    window.console.log("selectedQuantity: ", selectedQuantity);
-    if (selectedQuantity == 1 || selectedQuantity == 2 || selectedQuantity == 3) {
-        document.getElementById("textbox").innerHTML = [selectedQuantity.toString(), " kg cake added to cart!"];
-    } else {
-        document.getElementById("textbox").innerHTML = "";
-    }
-
-}
 
 //  Form validation methods
 function validateName() {
